641a74cef18ee0ca420fb81a5aae726158d3ec1c
Luke Yaegel <lyaegel@admins-MacBook-Pro-2.local>
2019-07-25 11:23:54 -0400

    Moved configs to central config repo

--- src/main/java/com/lukes/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/lukes/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -2,12 +2,14 @@ package com.lukes.microservices.limitsservice.controller;
 
 import com.lukes.microservices.limitsservice.model.LimitsConfiguration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lyaegel on 07/25/2019
  */
+@RefreshScope
 @RestController
 public class LimitsConfigurationController {
 
